69886078b61dc85d271603aa0262647ddc0008b9
Collins <upendocollins12@gmail.com>
2026-07-15 14:55:33 +0300

    fixed cors issue, added gitignore, fixed path issue

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
 import sqlite3
+
 
 app = FastAPI()
 
 
+# Gets the directory where the script is located
+root_dir = Path(__file__).resolve().parent.parent
+print(root_dir)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # Allow every origin (development only)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 def get_connection():
-    conn=sqlite3.connect("C:/Users/hp/song-recommender/database/song_recommendations_db.db")
+    conn=sqlite3.connect(f"{root_dir}/database/song_recommendations_db.db")
+    conn.row_factory = sqlite3.Row
     return conn
 
 
@@ -15,9 +32,13 @@ def all_songs():
     conn=get_connection()
     cursor=conn.cursor()
     cursor.execute("SELECT * FROM songs limit 50")
-    songs=cursor.fetchall()
+    songs= []
+
+    song_objects = cursor.fetchall()
     conn.close()
-    print(songs)
-    print(type(songs))
+
+    for s in song_objects:
+        songs.append(dict(s))
+
     return songs
 
